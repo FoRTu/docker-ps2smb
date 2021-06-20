d1ae44058daefe2f40d4bac9760286b8b1f22799
FoRTu <mikelfortuna@gmail.com>
2021-06-20 13:18:31 +0200

    🗑️ no more needed

--- run.sh
+++
@@ -1,5 +0,0 @@
-docker run --name='PS2smb' \
--p '10445:445/tcp' \
--v '/mnt/user/Otros/OPL/smb.conf':'/etc/samba/smb.conf':'rw' \
--v '/mnt/user/OPL':'/mnt/games':'rw' \
-'fortu/test_ps2smb:4.13.8'
